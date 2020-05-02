8811a493be1ee48da111d5c5cd1e7530963ed03c
Mark-Zeng <qz689@nyu.edu>
2020-05-02 16:51:36 +0800

    grouped itemsets

--- incPFP.py
+++ incPFP.py
@@ -1,9 +1,33 @@
 from pyspark import RDD, SparkConf, SparkContext
 from operator import add
+import math
 import os
 
+def genHashGmap(Flist: list, parNum: int):
+    gMap = {}
+    for i, t in enumerate(Flist):
+        gNum = i % parNum
+        gMap[gNum] = gMap.get(gNum, []) + [t]
+    return gMap
+
+# successive grouping
+def genSuccGMap(Flist: list, parNum: int):
+    #gLen = math.ceil(len(Flist) / parNum)
+    pass
+
+
+def groupMap(gMap, tran):
+    tranMap = {}
+    for i, t in enumerate(tran):
+        if t in gMap:
+            gid = gMap[t] # successive partition
+            tranMap[gid] = tran[:i + 1]
+    return [(k, v) for k, v in tranMap.items()]
+    
+
 if __name__ == '__main__':
-    conf = SparkConf().setAppName("incPFP")\
+    conf = SparkConf()\
+        .setAppName("incPFP")\
         .setMaster("local[*]")
     sc = SparkContext(conf = conf)
     inFile = "transData"
@@ -14,17 +38,25 @@ if __name__ == '__main__':
     
     # step 1 & 2: generate F-list
     transData = transDataFile.map(lambda r: r.split())
-    bigFlist = transData.flatMap(lambda l: [(i, 1) for i in l])\
+    bigFlist = transData\
+        .flatMap(lambda l: [(i, 1) for i in l])\
         .reduceByKey(add)\
         .filter(lambda i: i[1] >= minsup)\
         .sortBy(lambda i: i[1])\
         .collect()
 
     Flist = list(map(lambda t: t[0], bigFlist))
-    #print(Flist)
 
     # step 3: grouping items
-
+    gLen = math.ceil(len(Flist) / sc.defaultParallelism)
+    gMap = {t: math.floor(i / gLen) for i, t in enumerate(Flist)}
+    
+    groupedTrans = transData\
+        .flatMap(lambda t:groupMap(gMap, t))\
+        .groupByKey()\
+        .map(lambda t: (t[0], list(t[1])))\
+        .collect()
+    # print(groupedTrans)
 
     sc.stop()
     
