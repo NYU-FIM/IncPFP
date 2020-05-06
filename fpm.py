a14e6d32414b69e1e1698b3d2f9106f810a423c7
Mark-Zeng <qz689@nyu.edu>
2020-05-07 00:14:05 +0800

    finished fp-growth

--- fpm.py
+++ fpm.py
@@ -1,17 +1,23 @@
+import copy
+from itertools import chain, combinations
+
+# powerset with empty set excluded
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
+
 # a minimum fp-tree structure for gid-based
 # FPGrowth
 class FPTree:
     #constructor
-    def __init__(self, gid, flist):
+    def __init__(self, gid, flist, min_sup):
         self.gid = gid
         self.flist = flist # flist should be least freq->most freq
         self.root = Node('')
+        self.min_sup = min_sup
         self.linkListHead = {}
         self.linkListTail = {}
-        for i in flist:
-            newNode = Node(i)
-            self.linkListHead[i] = newNode
-            self.linkListTail[i] = newNode
     
     # private method to add an item in a transaction
     # returns the added node
@@ -22,7 +28,10 @@ class FPTree:
             return nodeToAdd
         newNode = Node(item, parent = where) # to modify
         where.addChild(newNode)
-        self.linkListTail[item].next = newNode
+        if item not in self.linkListHead:
+            self.linkListHead[item] = newNode
+        else:
+            self.linkListTail[item].next = newNode
         self.linkListTail[item] = newNode
         return newNode
 
@@ -35,7 +44,7 @@ class FPTree:
                 if j == i:
                     sortedTran.append(j)
 
-        print(sortedTran)
+        # print(sortedTran)
         return sortedTran
 
     def addTrans(self, newTran: list):
@@ -54,6 +63,71 @@ class FPTree:
             return
         for i in start.children:
             self._printsubtree(i)
+
+    def _getTran(self, start):
+        itemList = []
+        c = start.count
+        p = start.parent
+        while p.item != '':
+            itemList.append(p.item)
+            p = p.parent
+        return itemList, c
+
+    def _mineOneBranch(self, start):
+        occurrences = start.count
+        if occurrences < self.min_sup:
+            return []
+        itemList = []
+        p = start.parent
+        while p.item != '':
+            itemList.append(p.item)
+            p = p.parent
+        return powerset(itemList)
+
+    def _mineSingleItem(self, start):
+        item = start.item
+        newTrans = []
+        newflistDic = {}
+
+        s = start
+        while s is not None:
+            itemList, c = self._getTran(s)
+            for i in itemList:
+                newflistDic[i] = newflistDic.get(i, 0) + c
+            if len(itemList) != 0:
+                for _ in range(c):
+                    newTrans.append(itemList)
+            s = s.next
+        newflist = []
+        for k, v in newflistDic.items():
+            if v >= self.min_sup:
+                newflist.append((k, v))
+
+        newflist.sort(key = lambda k: k[1])
+        newTreeFlist = [i[0] for i in newflist]
+        print("node", start)
+        print("flist", newTreeFlist)
+
+        itemFpt = FPTree(self.gid, newTreeFlist, self.min_sup)
+        for t in newTrans:
+            itemFpt.addTrans(t)
+        if start.next is None:
+            return [(item, *i) for i in itemFpt._mineOneBranch(start)]
+        
+        print("sub-FPtree")
+        itemFpt.display()
+        return [(item, *i) for i in itemFpt.mine()]
+
+            
+
+    # return a list of tuples (freq itemsets)
+    def mine(self):
+        result = [(i, ) for i in self.flist]
+        for header in self.flist:
+            start = self.linkListHead[header]
+            # print(start)
+            result += self._mineSingleItem(start)
+        return result
 
 
 # Node structure for fp-tree nodes
@@ -96,10 +170,23 @@ if __name__ == '__main__':
     # [a, f, c, e, l, p, m, n]
     # min support = 3
     # f-list = [a, b, m, p, c, f]
-    fpt = FPTree(0, ['a', 'b', 'm', 'p', 'c', 'f'])
+    fpt = FPTree(0, ['a', 'b', 'm', 'p', 'c', 'f'], 3)
     fpt.addTrans(['f', 'a', 'c', 'd', 'g', 'i', 'm', 'p'])
     fpt.addTrans(['a', 'b', 'c', 'f', 'l', 'm', 'o'])
     fpt.addTrans(['b', 'f', 'h', 'j', 'o', 'w'])
     fpt.addTrans(['b', 'c', 'k', 's', 'p'])
     fpt.addTrans(['a', 'f', 'c', 'e', 'l', 'p', 'm', 'n'])
-    fpt.display()
+    fims = fpt.mine()
+    print(fims)
+
+
+
+    # fpt.display()
+
+    # for i in fpt.linkListHead.values():
+    #     start = i
+    #     while start.next is not None:
+    #         print(start)
+    #         start = start.next
+
+
