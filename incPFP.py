1b2c803c447b3eead7f248c75a3c8de5022aff39
Mark-Zeng <qz689@nyu.edu>
2020-05-08 14:28:55 +0800

    completed incPFP

--- incPFP.py
+++ incPFP.py
@@ -10,7 +10,7 @@ def sortEachTran(t, flist):
         for j in t:
             if j == i:
                 sortedTran.append(j)
-                
+
     return sortedTran
 
 def computFlist(trans, head, min_sup):
@@ -30,23 +30,39 @@ def computFlist(trans, head, min_sup):
     returnFlist = list(map(lambda t: t[0], flist))
     return returnFlist, newTrans
 
-
-def localFPM(gidTrans, gList, min_sup):
+def inclocalFPM(gidTrans, gList, min_sup, incFlist):
     gid, trans = gidTrans
     itemList = gList[gid]
     res = []
     #itemList is sorted already
     for i in itemList:
+        if i not in incFlist:
+            continue
         flist, newtrans = computFlist(trans, i, min_sup)
-        # print("item:", i)
-        # print("flist:", newtrans)
-        # print("trans", newtrans)
+        # local fp-tree is the conditional pattern base of item i
         localFpt = fpm.FPTree(gid, flist, min_sup)
         for t in newtrans:
             localFpt.addTrans(t)
-        res += [(i, *j) for j in localFpt.mine()]
-    # print("res", res)
-    return res + [(i,) for i in itemList]
+        # res.append((i, localFpt))
+        res += [(i, *j) for j in localFpt.mine()] 
+    return res
+
+
+# returns a list of tuples: (item, conditional FP-tree)
+def localFPM(gidTrans, gList, min_sup):
+    gid, trans = gidTrans
+    itemList = gList[gid]
+    res = [(i,) for i in itemList]
+    #itemList is sorted already
+    for i in itemList:
+        flist, newtrans = computFlist(trans, i, min_sup)
+        # local fp-tree is the conditional pattern base of item i
+        localFpt = fpm.FPTree(gid, flist, min_sup)
+        for t in newtrans:
+            localFpt.addTrans(t)
+        # better to find a way to store the tree?
+        res += [(i, *j) for j in localFpt.mine()] 
+    return res
 
 def genHashGmap(Flist: list, parNum: int):
     gMap = {}
@@ -54,12 +70,6 @@ def genHashGmap(Flist: list, parNum: int):
         gNum = i % parNum
         gMap[gNum] = gMap.get(gNum, []) + [t]
     return gMap
-
-# successive grouping
-def genSuccGMap(Flist: list, parNum: int):
-    #gLen = math.ceil(len(Flist) / parNum)
-    pass
-
 
 def groupMap(gMap, tran):
     tranMap = {}
@@ -77,9 +87,13 @@ if __name__ == '__main__':
     sc = SparkContext(conf = conf)
     inFile = "transData"
 
-    minsup = 2
+
+
+    minsupperc = 0.4
 
     transDataFile = sc.textFile(inFile)
+    numTrans = transDataFile.count()
+    minsup = minsupperc * numTrans
     
     # step 1: generate F-list
     transData = transDataFile.map(lambda r: r.split())
@@ -89,6 +103,7 @@ if __name__ == '__main__':
         .filter(lambda i: i[1] >= minsup)\
         .sortBy(lambda i: i[1])\
         .collect()
+    print(bigFlist)
 
     Flist = list(map(lambda t: t[0], bigFlist))
     
@@ -107,15 +122,49 @@ if __name__ == '__main__':
         .flatMap(lambda t:groupMap(gMap, t))\
         .groupByKey()\
         .map(lambda t: (t[0], list(t[1])))
-    print("gMap", gMap)
-    print("gList", gList)
-    print("Grouped Trans", groupedTrans.collect())
+    # print("gMap", gMap)
+    # print("gList", gList)
+    # print("Grouped Trans", groupedTrans.collect())
 
     # step 3: 
     staticRunFIs = groupedTrans.flatMap(lambda t:\
         localFPM(t, gList, minsup))
-    
     print(staticRunFIs.collect())
+
+    incTrans = sc.textFile('incData')
+
+    incTransData = incTrans.map(lambda r: r.split())
+    incBigFlist = incTransData\
+        .flatMap(lambda l: [(i, 1) for i in l])\
+        .reduceByKey(add)\
+        .filter(lambda i: i[1] >= minsup)\
+        .collect() # no need to sort for the incFlist
+
+    incFlist = list(map(lambda t: t[0], incBigFlist))
+    
+    # update global flist
+    for i, c in incBigFlist:
+        for ind, j in enumerate(bigFlist):
+            if j == i:
+                bigFlist[ind] = (j[0], j[1] + c)
+    
+
+    print(bigFlist)
+    totalTransData = transData.union(incTransData)
+
+    incGroupedTrans = totalTransData\
+        .map(lambda t: sortEachTran(t, Flist))\
+        .flatMap(lambda t:groupMap(gMap, t))\
+        .groupByKey()\
+        .map(lambda t: (t[0], list(t[1])))
+    newFIs = incGroupedTrans.flatMap(lambda t:\
+        inclocalFPM(t, gList, minsup, incFlist))
+
+    print(totalTransData.collect())
+    print(newFIs.collect())
+
+    #TODO: use heap if want to remove duplicates
+    # in the updated FIs
 
     sc.stop()
     
